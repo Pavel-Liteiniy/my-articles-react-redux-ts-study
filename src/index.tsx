48b049beabe2cdc8d5a58185f27fa5eeb38e40dd
Pavel-Liteiniy <paulsev96@gmail.com>
2021-06-08 23:12:49 +0300

    Подключает redux

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import { render } from 'react-dom';
+import { createStore, applyMiddleware, Store } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import App from './app';
+import reducer from './store/reducer';
+
+const store: Store<ArticleState, ArticleAction> & { dispatch: DispatchType } = createStore(
+	reducer,
+	applyMiddleware(thunk)
+);
+
+const rootElement = document.getElementById('root');
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
